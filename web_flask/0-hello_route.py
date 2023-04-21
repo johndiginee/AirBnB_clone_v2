38db9ac52cd1be549195189ff5794f8bcdd00dc5
John Diginee <johndbizz@gmail.com>
2023-04-21 06:54:49 +0000

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A script that starts a flask web application
-Your web application must be listening on 0.0.0.0, port 5000
+   listening on 0.0.0.0, port 5000
 """
 
 from flask import Flask
