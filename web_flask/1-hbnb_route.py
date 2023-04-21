92df2da78a7914e64cd943f1b97644d10dddea6c
John Diginee <johndbizz@gmail.com>
2023-04-21 07:03:00 +0000

    Update web_flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Write a script that starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
+    listening on 0.0.0.0, port 5000
 """
 
 from flask import Flask
